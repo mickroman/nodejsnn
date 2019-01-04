1f23bc5180770706cd8b3cc7471f460810c2bdcf
mick roman <mickroman65@gmail.com>
2019-01-04 00:49:26 +0000

    set interval, __dirname__filename

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var fs = require('fs');
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
 
-console.log('This is an instruction outside the sync file system.');
+
+console.log(--dirname);
+console.log(--filename);//console.log("Hey Ninjas");
+var time =0;
+
+setInterval(function(){
+    time +=2;
+    console.log(time + 'seconds have passed');
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+// var fs = require('fs');
+
+// var file = fs.readFile('input.txt','utf8',function(err,data){
+//     fs.writeFile('writeme.txt',data);
+//     console.log(data);
+// });
+
+// console.log('This is an instruction outside the sync file system.');
