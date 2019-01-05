a74e15bce29577c2273a910869b45a2e094b922d
mick roman <mickroman65@gmail.com>
2019-01-05 17:38:28 +0000

    node event emitter

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-var stuff = require('./stuff');
+var events = require('events');
 
-console.log(stuff.counter(['shaun', 'crystal','ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi,5));
+var util = require('util');
+
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james= new Person('james ');
+var mary= new Person('mary ');
+var ryu= new Person(' ryu ');
+var people= [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+        console.log (person.name +  'said : ' + mssg);
+    });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', ' I want a curry');
+
+
+
+
+
+//var myEmitter = new events.EventEmitter();
+//myEmitter.on('someEvent', function (mssg){
+ //console.log(mssg);  
+//});
+
+//myEmitter.emit('someEvent',' the event was emitted' );
+
+//console.log(stuff.counter(['shaun', 'crystal','ryu']));
+//console.log(stuff.adder(5,6));
+//console.log(stuff.adder(stuff.pi,5));
