164908352935a1135b295c10d12a282ebde7497f
mick roman <mickroman65@gmail.com>
2019-01-24 23:53:37 +0000

    version9.0

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-var events = require('events');
-
-var util = require('util');
-
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james= new Person('james ');
-var mary= new Person('mary ');
-var ryu= new Person(' ryu ');
-var people= [james, mary, ryu];
-
-people.forEach(function(person){
-    person.on('speak', function(mssg){
-        console.log (person.name +  'said : ' + mssg);
-    });
+var fs = require('fs'); 
+fs.readFile('readMe.txt','utf8', function(err, data){
+ console.log(data);   
 });
 
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', ' I want a curry');
 
-
-
-
-
-//var myEmitter = new events.EventEmitter();
-//myEmitter.on('someEvent', function (mssg){
- //console.log(mssg);  
-//});
-
-//myEmitter.emit('someEvent',' the event was emitted' );
-
-//console.log(stuff.counter(['shaun', 'crystal','ryu']));
-//console.log(stuff.adder(5,6));
-//console.log(stuff.adder(stuff.pi,5));
+console.log('test');
