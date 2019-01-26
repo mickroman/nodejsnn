e5890e83c3ced6343d368e0da9d2e95942a7d84b
mick roman <mickroman65@gmail.com>
2019-01-26 16:08:24 +0000

    version15.0

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var http = require('http');
 var fs = require('fs');
 //utf8 will deliver all data 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk recieved:');
-    console.log(chunk);
+   myWriteStream.write(chunk); 
+    
 });
-
-
-
 
 
 /*var server = http.createServer(function(req, res){
