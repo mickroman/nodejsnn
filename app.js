a4190c73f9d91a58e7eda73fae82aba7e404d5a2
mick roman <mickroman65@gmail.com>
2019-01-26 17:19:37 +0000

    version16.0

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var http = require('http'); 
 var fs = require('fs');
-//utf8 will deliver all data 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-
-myReadStream.on('data', function(chunk){
-    console.log('new chunk recieved:');
-   myWriteStream.write(chunk); 
-    
-});
+ 
 
 
-/*var server = http.createServer(function(req, res){
+
+
+var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
  res.writeHead(200, {'Content-Type': 'text/plain'});
- res.end('Hey ninjas');
+ var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+myReadStream.pipe(res);
+
  
 });
-server.listen(3000, '127.0.0.1');
+server.listen(8080, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
-*/
+
+
+// server.listen(process.env.PORT, process.env.IP);
+// console.log('yo dawgs, now listening to port 3000');
