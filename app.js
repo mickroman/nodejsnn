8a12cd4c07ba2ddfeaec659e67e5787e8a4f0286
mick roman <mickroman65@gmail.com>
2019-01-27 21:24:27 +0000

    version18.0

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
- res.writeHead(200, {'Content-Type': 'text/html'});
- var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-myReadStream.pipe(res);
+ res.writeHead(200, {'Content-Type': 'application/json'});
+ var myObj = {
+     name: 'Ryu',
+     job: 'Ninja',
+     age : 29
+ };
+ 
+ res.end(JSON.stringify(myObj));
+
 
  
 });
-server.listen(8080, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+//server.listen(3000, '127.0.0.1');
+//console.log('yo dawgs, now listening to port 3000');
 
 
-// server.listen(process.env.PORT, process.env.IP);
-// console.log('yo dawgs, now listening to port 3000');
+ server.listen(process.env.PORT, process.env.IP);
+ console.log('yo dawgs, now listening to port 3000');
